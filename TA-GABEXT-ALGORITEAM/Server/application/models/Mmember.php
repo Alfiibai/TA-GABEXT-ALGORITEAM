507734b2f21011ccd949465d63cd6ccbf54a682c
armelia002 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-07 21:24:07 +0700

    armel : upload Mmodel di model server

---
+++ TA-GABEXT-ALGORITEAM/Server/application/models/Mmember.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mmember extends CI_Model {
+
+	// buat method untuk tampil data
+    function get_data()
+    {
+
+        $this->db->select("id AS id,
+        nama_member AS nama_mbr,
+        email AS email,
+        no_telp AS no_telp,
+        ");
+
+        $this->db->from("member");
+        $this->db->order_by("id","ASC");
+
+        $query = $this->db->get()->result();
+        return $query;
+
+    }
+    
+    // buat fungsi untuk simpan data
+    function save_data($id,$nama_member,$email,$no_telp,$token)
+    {
+       
+       $this->db->select("id");
+       $this->db->from("member");
+       $this->db->where("TO_BASE64(id) = '$token'");
+       //eksekusi query
+       $query = $this->db->get()->result();
+       
+       if(count($query) == 0)
+       {
+         // isi nilai untuk masing2 field
+         $data = array(
+            "id" => $id,
+            "nama_member" => $nama_member,
+            "email" => $email,
+            "no_telp" => $no_telp,
+         );
+         // simpan data
+         $this->db->insert("member",$data);
+         $hasil= 0;
+
+       }
+
+      else
+      {
+        $hasil = 1;
+      }
+      return $hasil;
+    }
+
+
+    
+    
+}
